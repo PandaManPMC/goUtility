d239079bfdaa6b5201d9be824111ddd36254674f
PMC <pmc@gmail.com>
2025-10-11 15:05:32 +0800

    no message

--- util/httpUtil.go
+++ util/httpUtil.go
@@ -21,41 +21,95 @@ func GetInstanceByHttpUtil() *httpUtil {
 }
 
 // GetRequestIp 获取客户端 ip，绕过代理，会去除端口号
-func (*httpUtil) GetRequestIp(req *http.Request) string {
-	ip := func(req *http.Request) string {
+//func (*httpUtil) GetRequestIp(req *http.Request) string {
+//	ip := func(req *http.Request) string {
+//		// 优先使用 X-Forwarded-For
+//		fIp := req.Header.Get("X-Forwarded-For")
+//		if "" != fIp && !strings.Contains(fIp, "[") {
+//			// x.x.x.x,xx.xx.x.x,x.x.x.xx ...
+//			if strings.Contains(fIp, ",") {
+//				ips := strings.Split(fIp, ",")
+//				return ips[0]
+//			}
+//			return fIp
+//		}
+//
+//		rIp := req.RemoteAddr
+//		// RemoteAddr=[::1] or 127.0.0.1
+//		if "" != rIp && !strings.Contains(rIp, "[") && !strings.HasPrefix(rIp, "127.") {
+//			return rIp
+//		}
+//
+//		xIp := req.Header.Get("X-Real-IP")
+//		if "" != xIp && !strings.Contains(xIp, "[") {
+//			return xIp
+//		}
+//
+//		remoteAddr := req.Header.Get("Remote_addr")
+//		if "" != remoteAddr && !strings.Contains(remoteAddr, "[") {
+//			return remoteAddr
+//		}
+//
+//		return req.RemoteAddr
+//	}(req)
+//	if strings.Contains(ip, ":") {
+//		return strings.Split(ip, ":")[0]
+//	}
+//	return strings.Trim(ip, " ")
+//}
+
+// GetRequestIp 获取客户端 ip，绕过代理，会去除端口号
+func (*httpUtil) GetRequestIp(req http.Header) string {
+	ip := func(req http.Header) string {
 		// 优先使用 X-Forwarded-For
-		fIp := req.Header.Get("X-Forwarded-For")
-		if "" != fIp && !strings.Contains(fIp, "[") {
+		fIp := req.Get("X-Forwarded-For")
+		if "" != fIp {
+			if strings.Contains(fIp, "[") {
+				fIp = strings.ReplaceAll(fIp, "[", "")
+				fIp = strings.ReplaceAll(fIp, "]", "")
+			}
+
 			// x.x.x.x,xx.xx.x.x,x.x.x.xx ...
 			if strings.Contains(fIp, ",") {
 				ips := strings.Split(fIp, ",")
-				return ips[0]
+				return strings.TrimSpace(ips[0])
 			}
-			return fIp
+			return strings.TrimSpace(fIp)
 		}
 
-		rIp := req.RemoteAddr
+		rIp := req.Get("RemoteAddr")
+		if strings.Contains(rIp, "[") {
+			rIp = strings.ReplaceAll(rIp, "[", "")
+			rIp = strings.ReplaceAll(rIp, "]", "")
+		}
+
 		// RemoteAddr=[::1] or 127.0.0.1
-		if "" != rIp && !strings.Contains(rIp, "[") && !strings.HasPrefix(rIp, "127.") {
+		if "" != rIp && !strings.HasPrefix(rIp, "127.") {
 			return rIp
 		}
 
-		xIp := req.Header.Get("X-Real-IP")
-		if "" != xIp && !strings.Contains(xIp, "[") {
+		xIp := req.Get("X-Real-IP")
+		if strings.Contains(xIp, "[") {
+			xIp = strings.ReplaceAll(xIp, "[", "")
+			xIp = strings.ReplaceAll(xIp, "]", "")
+		}
+		if "" != xIp {
 			return xIp
 		}
 
-		remoteAddr := req.Header.Get("Remote_addr")
-		if "" != remoteAddr && !strings.Contains(remoteAddr, "[") {
+		remoteAddr := req.Get("Remote_addr")
+		if strings.Contains(remoteAddr, "[") {
+			remoteAddr = strings.ReplaceAll(remoteAddr, "[", "")
+			remoteAddr = strings.ReplaceAll(remoteAddr, "]", "")
+		}
+		if "" != remoteAddr {
 			return remoteAddr
 		}
 
-		return req.RemoteAddr
+		return req.Get("RemoteAddr")
 	}(req)
-	if strings.Contains(ip, ":") {
-		return strings.Split(ip, ":")[0]
-	}
-	return strings.Trim(ip, " ")
+
+	return strings.ReplaceAll(ip, " ", "")
 }
 
 // UserAgent 解析 userAgent
